a5dc478da3f8160382229bcc726b29c92b458110
toby" radiation "fox <166326616+radishcatish@users.noreply.github.com>
2025-02-07 19:47:03 -0500

    temp stuff

---
+++ tempcam.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+@onready var wip_copy: Sprite2D = $"../../CanvasLayer/Wip-Copy"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+
+
+
+	
+	print(zoom)
