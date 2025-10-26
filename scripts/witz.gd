bbaff57d12e5b1eaebb68e03517e5f37514eb5e9
toby" radiation "fox <166326616+radishcatish@users.noreply.github.com>
2025-10-26 18:19:57 -0400

    tending to witz sprites

--- scripts/witz.gd
+++ scripts/witz.gd
@@ -16,8 +16,9 @@ var visual_dir:      int = 1
 var extra_speed:     float = 0.0
 var old_wall_normal: float = 0.0
 var wall_normal:     float = 0.0
-var lock_dir:  bool = true
-var lock_slow: bool = true
+var lock_dir:  bool = false
+var lock_slow: bool = false
+var lock_movement: bool = false
 const WALK_ACCEL = 100.0
 const AIR_ACCEL = 60.0
 const MAX_SPEED = 500.0
@@ -27,7 +28,7 @@ enum PlayerState {GENERAL, JAB}
 var State := PlayerState.GENERAL
 var AltState: int = 0
 func _physics_process(_delta: float) -> void:
-	var direction := int(Input.get_axis("left", "right"))
+	var direction := int(Input.get_axis("left", "right")) if not lock_movement else 0
 	var accel = WALK_ACCEL if is_on_floor() else AIR_ACCEL
 	last_on_floor = last_on_floor + 1 if not is_on_floor() else 0
 	last_on_wall = last_on_wall + 1 if not is_on_wall() else 0
@@ -45,8 +46,8 @@ func _physics_process(_delta: float) -> void:
 		old_wall_normal = get_wall_normal().x
 
 		
-	if last_z_press < 4:
-		State = PlayerState.GENERAL
+	if last_z_press < 4 and not lock_movement:
+		last_z_press = 4
 		if last_on_wall < 10 and not last_on_floor < 7 and old_wall_normal != wall_normal:
 			velocity.x += sign(old_wall_normal) * 1200
 			velocity.y = JUMP_VELOCITY
@@ -68,24 +69,32 @@ func _physics_process(_delta: float) -> void:
 	velocity.y = -200.0 if Input.is_action_just_released("z") and velocity.y < -200.0 else velocity.y
 	
 	# main movement
-	if not lock_slow:
-		velocity.x = move_toward(velocity.x, direction * (MAX_SPEED + extra_speed), accel)
+	if not lock_movement:
+		if not lock_slow:
+			velocity.x = move_toward(velocity.x, direction * (MAX_SPEED + extra_speed), accel)
+		else:
+			velocity.x = move_toward(velocity.x, direction * (AIR_ACCEL), accel)
 	else:
-		velocity.x = move_toward(velocity.x, direction * (AIR_ACCEL), accel)
+		velocity.x = move_toward(velocity.x, 0, accel)
 	
-	
-	if Input.is_action_just_pressed("x"):
-		State = PlayerState.JAB
-		sprite.play("jab1")
-		lock_dir = true
-		lock_slow = true
-		await sprite.animation_finished
-		lock_dir = false
-		lock_slow = false
-		State = PlayerState.GENERAL
 	
 	
 	move_and_slide()
+	
+	
+	
+	if is_on_floor() and last_x_press < 3:
+		State = PlayerState.JAB
+		sprite.play("jab")
+		lock_dir = true
+		lock_movement = true
+		await sprite.animation_finished
+		lock_dir = false
+		lock_movement = false
+		State = PlayerState.GENERAL
+	
+	
+
 	
 	sprite.flip_h = false if visual_dir == 1 else true
 	sprite.rotation_degrees = lerpf(0, clamp(velocity.x / 100, -18, 18), 1.5) if not is_on_floor() else 0
@@ -98,6 +107,8 @@ func _physics_process(_delta: float) -> void:
 						sprite.play("run")
 					else:
 						sprite.play("walk")
+					if ((direction == 1 and velocity.x < 0) or (direction == -1 and velocity.x > 0)) and abs(velocity.x) > 22:
+						sprite.play("skid")
 				else:
 					sprite.play("idle")
 				if not direction and Input.is_action_pressed("down"):
